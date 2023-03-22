8e6ea411f5bc0e70828823a832a6172fa628e0f0
Meghana Pocha Reddy <meghana.pochared@ufl.edu>
2023-03-22 18:04:10 -0400

    added decode function

--- encProgram.py
+++ encProgram.py
@@ -1,4 +1,5 @@
 #partner: meghana
+#Kathy's encoder code
 
 def encodeNum(encNum): # encodes the users password by shifting each digits by 3
     s2split = str(encNum)
@@ -11,6 +12,14 @@ def encodeNum(encNum): # encodes the users password by shifting each digits by 3
         encList.append(str(newNum))
     encodedNum = ''.join(encList)
     return encodedNum
+
+def decodeNum(decNum):
+    decoded_password = ""
+    for num in range(len(decNum)):  # for loop used to add 3 to each individual character in input
+        decoded_val = int(decNum[num]) - 3  # user's string input converted to int using int() to facilitate addition
+        decoded_password += str(decoded_val)
+    return decoded_password  # decode() function returns the final decoded password
+
 
 def displayMenu(): # displaying the menu options
     print("\nMenu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
@@ -32,7 +41,10 @@ def main(): # main function that calls the required functions and delegate menu 
             userOption = int(input("Please enter an option: "))
 
         if userOption == 2:  # displays the original password and the encoded password
-            pass # allows code to run and functions placeholder for decoder
+            decNum = encodeNum(encNum)
+            print(f"The encoded password is {encodeNum(encNum)} and the original password is {decodeNum(decNum)}.")
+            # f string displays the encoded  password and the original password
+            break # used to exit the while loop
 
         if userOption == 3: # exits program
             exit()
