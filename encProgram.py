b8f38b9e4c812fae8e64888d498b87a28d8e4bb1
Kathy Tran <ktran2@ufl.edu>
2023-03-22 14:00:16 -0400

    adding the encoder to the project

--- encProgram.py
+++ encProgram.py
@@ -23,18 +23,16 @@ def main(): # main function that calls the required functions and delegate menu 
 
     while 1 != 120983:
         if userOption == 1:  # takes the users password, stores it, and then calls function to encode it
-            encNum = int(input("Please enter your password to encode: "))
-            ogPW = str(encNum)
-            print("Your password has been encoded and stored!")
-            print(ogPW)
-            print(encodeNum(encNum))
+            encNum = str(int(input("Please enter your password to encode: ")))
+            if len(encNum) == 8:
+                ogPW = str(encNum)
+                encodeNum(encNum)
+                print("Your password has been encoded and stored!")
             displayMenu()
             userOption = int(input("Please enter an option: "))
 
         if userOption == 2:  # displays the original password and the encoded password
-            print(f"The encoded password is {encodeNum(encNum)}, and the original password is {ogPW}.")
-            displayMenu()
-            userOption = int(input("Please enter an option: "))
+            pass # allows code to run and functions placeholder for decoder
 
         if userOption == 3: # exits program
             exit()
